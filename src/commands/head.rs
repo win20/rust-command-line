8c83afebba2c666f3beb4a7a21fa384669d7e684
Win Barua <win.barua2@gmail.com>
2023-06-26 23:44:32 +0100

    Cleanup

--- src/commands/head.rs
+++ src/commands/head.rs
@@ -3,10 +3,10 @@ use std::process;
 pub use crate::helpers;
 
 pub fn head(args: Vec<String>) {
-    let mut option: Option<String> = None;
     let default_number_of_lines = 10;
-
     let files: Vec<String>;
+    let mut option: Option<String> = None;
+
     let mut option_index = args.len();
     for i in 0..args.len() {
         if args[i].starts_with("-") {
@@ -15,6 +15,7 @@ pub fn head(args: Vec<String>) {
         }
     }
     files = args[0..option_index].to_vec();
+
     match option {
         Some(_) => {
             let num_of_lines_to_read: u16 = match args[option_index + 1].parse::<u16>() {
@@ -26,6 +27,8 @@ pub fn head(args: Vec<String>) {
             };
 
             for file in files {
+                println!();
+                println!("==> {} <==", file);
                 match helpers::read_number_of_lines(&file, num_of_lines_to_read) {
                     Ok(lines) => {
                         for line in lines {
@@ -38,6 +41,8 @@ pub fn head(args: Vec<String>) {
         }
         None => {
             for file in files {
+                println!();
+                println!("==> {} <==", file);
                 match helpers::read_number_of_lines(&file, default_number_of_lines) {
                     Ok(lines) => {
                         for line in lines {
