bd4e934235dd666c6a82612742d6b5b9e90d99fe
Win Barua <win.barua2@gmail.com>
2023-06-28 19:23:48 +0100

    Print last part of file in order

--- src/commands/tail.rs
+++ src/commands/tail.rs
@@ -3,7 +3,7 @@ use crate::helpers;
 pub fn tail() {
     let lines = helpers::read_number_of_lines_in_reverse("test.txt", 2);
 
-    for line in lines.unwrap() {
-        println!("{:?}", line);
+    for line in lines.unwrap().iter().rev() {
+        println!("{}", line.as_str());
     }
 }
