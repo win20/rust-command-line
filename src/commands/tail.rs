8837f4ee2798313702d116c5445213d13fe58905
Win Barua <win.barua2@gmail.com>
2023-06-28 19:09:49 +0100

    Set up helper function to read file in reverse

--- src/commands/tail.rs
+++ src/commands/tail.rs
@@ -1,18 +1,9 @@
-use std::io::{self, BufRead};
-
-use rev_buf_reader::RevBufReader;
+use crate::helpers;
 
 pub fn tail() {
-    // let default_number_of_lines = 10;
-    // let files: Vec<String>;
-    // let mut option: Option<String> = None;
+    let lines = helpers::read_number_of_lines_in_reverse("test.txt", 2);
 
-    let data = "a\nb\nc";
-    let inner = io::Cursor::new(&data);
-    let reader = RevBufReader::new(inner);
-    let mut lines = reader.lines();
-
-    for line in lines {
-        println!("{}", line.unwrap());
+    for line in lines.unwrap() {
+        println!("{:?}", line);
     }
 }
--- src/helpers.rs
+++ src/helpers.rs
@@ -1,3 +1,4 @@
+use rev_buf_reader::RevBufReader;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -15,6 +16,22 @@ pub fn read_number_of_lines(file: &String, num_of_lines: u16) -> io::Result<Vec<
     let mut lines_to_return: Vec<String> = Vec::new();
 
     for _i in 0..=num_of_lines {
+        match lines.next() {
+            Some(line) => lines_to_return.push(line.unwrap()),
+            None => return Ok(lines_to_return),
+        }
+    }
+
+    Ok(lines_to_return)
+}
+
+pub fn read_number_of_lines_in_reverse(file: &str, num_of_lines: u16) -> io::Result<Vec<String>> {
+    let mut lines_to_return: Vec<String> = Vec::new();
+    let file = File::open(file)?;
+    let reader = RevBufReader::new(file);
+    let mut lines = reader.lines();
+
+    for _i in 0..num_of_lines {
         match lines.next() {
             Some(line) => lines_to_return.push(line.unwrap()),
             None => return Ok(lines_to_return),
