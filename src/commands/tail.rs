8407d164934139f70baded7bf4dfd0615d392d3d
Win Barua <win.barua2@gmail.com>
2023-06-29 22:14:56 +0100

    Tail now takes in options

--- src/commands/tail.rs
+++ src/commands/tail.rs
@@ -1,20 +1,57 @@
+use std::process;
+
 use crate::helpers;
 
 pub fn tail(args: Vec<String>) {
-    let default_number_of_lines = 10;
-    let files: Vec<String> = args;
+    let default_number_of_lines = 5;
+    let files: Vec<String>;
+    let mut option: Option<String> = None;
 
-    // let lines = helpers::read_number_of_lines_in_reverse("test.txt", 2);
+    let mut option_index = args.len();
+    for i in 0..args.len() {
+        if args[i].starts_with("-") {
+            option_index = i;
+            option = Some(args[option_index].clone());
+        }
+    }
+    files = args[0..option_index].to_vec();
 
-    for file in files {
-        println!();
-        match helpers::read_number_of_lines_in_reverse(&file, 1) {
-            Ok(lines) => {
-                for line in lines {
-                    println!("{}", &line);
+    match option {
+        Some(_) => {
+            let num_of_lines_to_read: u16 = match args[option_index + 1].parse::<u16>() {
+                Ok(int) => int,
+                Err(e) => {
+                    println!("Error: {}", e);
+                    process::exit(1);
+                }
+            };
+
+            for file in files {
+                println!();
+                println!("==> {} <==", file);
+                match helpers::read_number_of_lines_in_reverse(&file, num_of_lines_to_read) {
+                    Ok(lines) => {
+                        for line in lines.iter().rev() {
+                            println!("{}", &line);
+                        }
+                    }
+                    Err(e) => println!("{}", e),
                 }
             }
-            Err(e) => println!("{}", e),
+        }
+        None => {
+            for file in files {
+                println!();
+                println!("==> {} <==", file);
+                match helpers::read_number_of_lines_in_reverse(&file, default_number_of_lines) {
+                    Ok(lines) => {
+                        for line in lines.iter().rev() {
+                            println!("{}", &line);
+                        }
+                    }
+                    Err(e) => println!("Error: {}", e),
+                }
+            }
         }
     }
 
